c15042492d04eef50c2a56fb364557a1710d65af
Kevin Narang <narangkevin@gmail.com>
2022-06-04 22:30:51 +0700

    fixed scrollview problem

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -32,88 +32,93 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Mtel Assignment'),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 750.0,
-              child: ListView(
-                children: [
-                  SizedBox(
-                    height: 150.0,
-                    width: double.infinity,
-                    child: Carousel(
-                      dotSize: 4.0,
-                      dotSpacing: 15.0,
-                      dotColor: Colors.amber,
-                      indicatorBgPadding: 2.0,
-                      dotBgColor: Colors.transparent,
-                      autoplay: false,
-                      images: [
-                        InkWell(
-                          child: Image.asset(
-                            'assets/img1.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const FormPage(
-                                  imgAddress: 'assets/img1.jpg',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 750.0,
+                child: ListView(
+                  children: [
+                    SizedBox(
+                      height: 150.0,
+                      width: double.infinity,
+                      child: Carousel(
+                        dotSize: 4.0,
+                        dotSpacing: 15.0,
+                        dotColor: Colors.amber,
+                        indicatorBgPadding: 2.0,
+                        dotBgColor: Colors.transparent,
+                        autoplay: false,
+                        images: [
+                          InkWell(
+                            child: Image.asset(
+                              'assets/img1.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const FormPage(
+                                    imgAddress: 'assets/img1.jpg',
+                                  ),
                                 ),
-                              ),
-                            );
-                          },
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const FormPage(
-                                    imgAddress: 'assets/img2.jpg'),
-                              ),
-                            );
-                          },
-                          child: Image.asset(
-                            'assets/img2.jpg',
-                            fit: BoxFit.cover,
+                              );
+                            },
                           ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const FormPage(
-                                    imgAddress: 'assets/img3.jpg'),
-                              ),
-                            );
-                          },
-                          child: Image.asset(
-                            'assets/img3.jpg',
-                            fit: BoxFit.cover,
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const FormPage(
+                                      imgAddress: 'assets/img2.jpg'),
+                                ),
+                              );
+                            },
+                            child: Image.asset(
+                              'assets/img2.jpg',
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                      ],
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const FormPage(
+                                      imgAddress: 'assets/img3.jpg'),
+                                ),
+                              );
+                            },
+                            child: Image.asset(
+                              'assets/img3.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: 100,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        shadowColor: Colors.grey,
-                        child: ListTile(
-                          title: Text('$index'),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                    Expanded(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 100,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            shadowColor: Colors.grey,
+                            child: ListTile(
+                              title: Text('$index'),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
